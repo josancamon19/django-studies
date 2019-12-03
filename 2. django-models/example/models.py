abca6b0c536c409d650e585a0e7bc7eb69cff698
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-12-02 23:39:51 -0500

    Default values added to models DateTime

--- 2. django-models/example/models.py
+++ 2. django-models/example/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from django.utils.timezone import now
 
 # Create your models here.
 class Company(models.Model):  # company.programmer_set.all() returns all programmers in this company
     name = models.CharField(max_length=20)
     location = models.CharField(max_length=20)
-    date_created = models.DateField()
+    date_created = models.DateField(default=now())
 
     def __str__(self):
         return self.name
@@ -15,7 +15,7 @@ class Language(models.Model):
     name = models.CharField(max_length=20)
     creator = models.CharField(max_length=20)
     paradigm = models.CharField(max_length=20)
-    date_created = models.DateField()
+    date_created = models.DateField(default=now())
 
     def __str__(self):
         return self.name
