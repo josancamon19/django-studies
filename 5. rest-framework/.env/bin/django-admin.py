ce4f4f118361d70c5fcad8d0dcfe6bc19766d6c2
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-12-04 20:03:57 -0500

    Rest Framework Series Started

---
+++ 5. rest-framework/.env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/josancamon19/Downloads/personal/django/5. rest-framework/.env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
