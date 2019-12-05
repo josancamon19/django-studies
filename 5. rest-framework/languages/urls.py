75a0fb227e3a2adb1fa9c5acbf2a02ae36b8eab5
josancamon19 <joan.santiago.cabezas@gmail.com>
2019-12-04 21:34:57 -0500

    Rest Framework Series 2 completed

--- 5. rest-framework/languages/urls.py
+++ 5. rest-framework/languages/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 router = routers.DefaultRouter()
 router.register('languages', views.LanguageView)
+router.register('paradigms', views.ParadigmView)
+router.register('programmers', views.ProgrammerView)
 
 urlpatterns = [
     path('', include(router.urls))
